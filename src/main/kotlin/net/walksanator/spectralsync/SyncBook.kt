1b45d8a21157e0996300b2782b90934cfff83860
Andrew Pillsbury <and.pillsbury@gmail.com>
2024-11-18 15:36:18 -0500

    Update SyncBook.kt

--- src/main/kotlin/net/walksanator/spectralsync/SyncBook.kt
+++ src/main/kotlin/net/walksanator/spectralsync/SyncBook.kt
@@ -8,13 +8,11 @@ import net.minecraft.resources.ResourceLocation
 import net.minecraft.server.level.ServerLevel
 import net.minecraft.server.level.ServerPlayer
 import net.minecraft.world.InteractionHand
-import net.minecraft.world.InteractionResult
 import net.minecraft.world.InteractionResultHolder
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.Level
-import kotlin.jvm.optionals.getOrNull
 
 class SyncBook(properties: Properties) : Item(properties) {
     override fun use(
@@ -23,8 +21,8 @@ class SyncBook(properties: Properties) : Item(properties) {
         interactionHand: InteractionHand
     ): InteractionResultHolder<ItemStack> {
         val stack = player.getItemInHand(interactionHand)
-        if (level.isClientSide()) {
-            return super.use(level,player,interactionHand)
+        if (level.isClientSide) {
+            return super.use(level, player, interactionHand)
         }
         level as ServerLevel
         player as ServerPlayer
@@ -34,26 +32,28 @@ class SyncBook(properties: Properties) : Item(properties) {
         }
 
         val adv = player.advancements
-        val srvAdv  = level.server.advancements
+        val srvAdv = level.server.advancements
         val tag = stack.tag?.getList("advancements", StringTag.TAG_STRING.toInt())
-        if ( (tag?.size?: 0) == 0 || player.isShiftKeyDown) {
+
+        if ((tag?.size ?: 0) == 0 || player.isShiftKeyDown) {
             val advStrs = srvAdv.allAdvancements.filter {
                 val prog = adv.getOrStartProgress(it)
                 prog.isDone && it.id.namespace == SpectrumCommon.MOD_ID
-            }.map {it.id.toString()}
+            }.map { it.id.toString() }
+
             val list = ListTag()
             advStrs.forEach {
                 list.add(StringTag.valueOf(it))
             }
-            stack.tag?.put("advancements",list)
+            stack.tag?.put("advancements", list)
         } else {
             tag!!.map { (it as StringTag).asString }
-                .mapNotNull { ResourceLocation.read(it).result().getOrNull() }
-                .mapNotNull {srvAdv.getAdvancement(it)}
-                .forEach {
-                    val prog = adv.getOrStartProgress(it)
-                    prog.remainingCriteria.forEach { crit ->
-                        adv.award(it,crit)
+                .mapNotNull { ResourceLocation.tryParse(it) }
+                .mapNotNull { srvAdv.getAdvancement(it) }
+                .forEach { advancement ->
+                    val progress = adv.getOrStartProgress(advancement)
+                    progress.remainingCriteria.forEach { criteria ->
+                        adv.award(advancement, criteria)
                     }
                 }
         }
@@ -62,7 +62,7 @@ class SyncBook(properties: Properties) : Item(properties) {
     }
 
     override fun isFoil(itemStack: ItemStack): Boolean {
-        val tag = itemStack.tag?.getList("advancements", StringTag.TAG_STRING.toInt())?.size?: 0
-        return tag > 0
+        val tagSize = itemStack.tag?.getList("advancements", StringTag.TAG_STRING.toInt())?.size ?: 0
+        return tagSize > 0
     }
 }
